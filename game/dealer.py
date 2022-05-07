b3eb42bbc126ba209f6a0eaf2feef6026abf4c71
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-05-07 13:00:29 -0500

    Update dealer.py
    Now you can open run the file when previous where problems with attributes card.

--- game/dealer.py
+++ game/dealer.py
@@ -8,19 +8,17 @@ from game.card import Card
 
 class Dealer:
     """A person who directs the game. 
-
     The responsibility of a Dealer is to control the sequence of play and draw cards.
-
     Attributes:
         is_playing (boolean): Whether or not the game is being played.
     """
 
     def __init__(self):
         """Constructs a new Director.
-
         Args:
             self (Director): an instance of Director.
         """
+        self.card = []
         self.is_playing = True
         self.points = 0
         self.total_score = 300
@@ -28,35 +26,35 @@ class Dealer:
 
     def start_game(self):
         """Starts the game by running the main game loop.
-
         Args:
             self (Director): an instance of Director.
         """
         while self.is_playing:
-            #self.get_inputs()
+            self.get_inputs()
             self.do_updates()
             self.do_outputs()
 
     #def get_inputs(self):
         """Ask the user if they want to continue playing.
-
         Args:
             self (Dealer): An instance of Dealer.
         """
-
+    def get_inputs(self):
+        draw_card = input('Play again [y/n] ')
+        self.is_playing = (draw_card == 'y')
 
 
     def do_updates(self):
         """Updates the player's score.
-
         Args:
             self (Director): An instance of Director.
         """
         if not self.is_playing:
             return
-
-        card.draw()
-        self.points = card.points
+        for i in range(len(self.card)):
+         card = self.card[i]
+         card.draw()
+         self.points = card.points
         self.total_score += self.points
      
         
@@ -65,16 +63,21 @@ class Dealer:
 
     def do_outputs(self):
         """Displays the dice and the score. Also asks the player if they want to roll again. 
-
         Args:
             self (Director): An instance of Director.
         """
         if not self.is_playing:
-            return
+            return 
+        values = ""
+        for i in range(len(self.card)):
+           card = self.card[i]
+           values += f"{card.value} "
 
-        next_card = card.value
-
-        print(f'Next card was: {next_card}')
+        
+        print(f'Next card was: {values}')
         print(f'Your score is: {self.total_score}')
-        draw_card = input('Play again [y/n] ')
-        self.is_playing = (draw_card == 'y')
+        self.is_playing == (self.points > 0)
+        
+
+
+    
