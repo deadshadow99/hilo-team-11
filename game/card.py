05a9d36be5729c6dd9d54627c9fffc3dd4986690
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-05-09 22:40:42 -0500

    Update card.py

--- game/card.py
+++ game/card.py
@@ -1,5 +1,3 @@
-import random
-
 class Card:
     """The dealer draws a card that contains a random number between 1 and 13. 
     
@@ -10,40 +8,21 @@ class Card:
     """
 
     def __init__(self):
-        """Constructs a new instance of Card
-        
-        Args:
-            self (Card): an instance of Card
-        """
 
-        self.value = 0
-        self.points = 0
-        self.previous_value = 0
-        self.guess = ''
-        
+      self.guess = True
+      self.play_again = True
 
 
+    def get_input(self):
         
-    def draw(self):
-        """Generates a random value between 1 and 13 and calculates points based on player's guess.txt
+        while True:
+            answer = input("Higher or lower? [h/l]: ").lower()
 
-        Args:
-            self (Card): An instance of Card
-        
-        """
-
-        self.previous_value = random.randint(1, 13)
-        print(f'The card is {self.previous_value}')
-        guess = input('Higher or lower [h/l]')
-        self.value = random.randint(1, 13)
-        if self.value > self.previous_value and guess == 'h':
-            self.points = 100
-        elif self.value < self.previous_value and guess == 'l':
-            self.points = 100
-        elif self.value > self.previous_value and guess == 'l':
-            self.points = -75
-        elif self.value < self.previous_value and guess == 'h':
-            self.points = -75
-        else:
-            self.points = 0
-        
+            if answer == 'h':
+                self.guess = True
+                break
+            elif answer == 'l':
+                self.guess = False
+                break
+            else:
+                print("Invalid input, try again.")
