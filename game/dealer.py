534e2ac609c11f9c8a82acf22ce29d7fa45fdcb8
deadshadow99 <97075743+deadshadow99@users.noreply.github.com>
2022-05-10 20:48:15 -0500

    Update dealer.py

--- game/dealer.py
+++ game/dealer.py
@@ -8,72 +8,33 @@ from game.card import Card
 
 class Dealer:
     """A person who directs the game. 
-
     The responsibility of a Dealer is to control the sequence of play and draw cards.
-
     Attributes:
         is_playing (boolean): Whether or not the game is being played.
     """
 
     def __init__(self):
-        """Constructs a new Director.
-
+        """Constructs a new Dealer.
         Args:
-            self (Director): an instance of Director.
+            self (Dealer): an instance of Dealer.
         """
         self.is_playing = True
         self.points = 0
         self.total_score = 300
-        
+        self.card = Card()
 
     def start_game(self):
         """Starts the game by running the main game loop.
-
         Args:
-            self (Director): an instance of Director.
+            self (Dealer): an instance of Dealer.
         """
         while self.is_playing:
-            #self.get_inputs()
+            print('Welcome to Hilo game!')
             self.do_updates()
             self.do_outputs()
+            self.get_inputs()
 
-    #def get_inputs(self):
-        """Ask the user if they want to continue playing.
-
-        Args:
-            self (Dealer): An instance of Dealer.
-        """
-
-
-
-    def do_updates(self):
-        """Updates the player's score.
-
-        Args:
-            self (Director): An instance of Director.
-        """
-        if not self.is_playing:
-            return
-
-        card.draw()
-        self.points = card.points
-        self.total_score += self.points
-     
-        
-
-
-    def do_outputs(self):
-        """Displays the dice and the score. Also asks the player if they want to roll again. 
-        Args:
-            self (Dealer): An instance of Dealer.
-        """
-        if not self.is_playing:
-            return
-
-        next_card = self.card.value
-
-        print(f'Next card was: {next_card}')
-        print(f'Your score is: {self.total_score}')
+    def get_inputs(self):
         while True:
                 looping = input('Play again? [y/n]: ').lower()
                 if looping == 'y':
@@ -88,3 +49,35 @@ class Dealer:
                     print('Invalid input, try again!')
         print('')  
         pass
+
+
+    def do_updates(self):
+        """Updates the player's score.
+        Args:
+            self (Dealer): An instance of Dealer.
+        """
+        if not self.is_playing:
+            return
+
+        self.card.draw()
+        self.points = self.card.points
+        self.total_score += self.points
+     
+        
+
+
+
+    def do_outputs(self):
+        """Displays the dice and the score. Also asks the player if they want to roll again. 
+        Args:
+            self (Dealer): An instance of Dealer.
+        """
+        if not self.is_playing:
+            return
+
+        next_card = self.card.value
+
+        print(f'Next card was: {next_card}')
+        print(f'Your score is: {self.total_score}')
+        pass
+      
